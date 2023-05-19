13ea42d6c309417b79d9334f6c9bc6123ee438d8
Bruce Wang <bruceloveapple@gmail.com>
2023-05-19 16:55:57 +0800

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import db
 import os
 
-databaseURL = os.getenv("TRACKER_TELEGRAM_TOKEN")
+databaseURL = os.getenv("DATABASE_URL")
 
 cred = credentials.Certificate("accounts/firebase_account.json")
 firebase_admin.initialize_app(cred, {
